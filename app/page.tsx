8dd6ba246c69a4abd090203ddb8311783fd51316
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-10-28 19:20:38 +0530

    feat: shadcn setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div className="flex justify-center items-center h-screen text-4xl font-bold gap-2.5">
-      <h1>Rajput</h1>
-      <h1>Genset</h1>
-      <h1>& Solar</h1>
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-2">
+          <h1>Rajput</h1>
+          <h1>Genset</h1>
+          <h1>& Solar</h1>
+        </div>
+        <Button>Go to dashboard</Button>
+      </div>
     </div>
   );
 };
